079ea208faa786287355017dba660f5bdd9dde68
Rehan-Shah12 <rehannaqvi835@gmail.com>
2024-01-29 18:26:06 +0500

    Minor fixes

--- controllers/authController.js
+++ controllers/authController.js
@@ -27,20 +27,16 @@ export const registerUserController = async (req, res) => {
     const existingPhone = await User.findOne({ phone });
 
     if (existingUser) {
-      return res
-        .status(400)
-        .json({
-          error: "Email is Already in use",
-          message: "Given Email Already Exist ",
-        });
+      return res.status(400).json({
+        error: "Email is Already in use",
+        message: "Given Email Already Exist ",
+      });
     }
     if (existingPhone) {
-      return res
-        .status(400)
-        .json({
-          error: "Given Phone# Already in use",
-          message: "Given Phone Number Already Exist ",
-        });
+      return res.status(400).json({
+        error: "Given Phone# Already in use",
+        message: "Given Phone Number Already Exist ",
+      });
     }
 
     const user = new User({ name, email, phone, password: secretPass });
@@ -59,7 +55,7 @@ export const registerUserController = async (req, res) => {
 export const loginUserController = async (req, res) => {
   try {
     const { identifier, password } = req.body;
-    console.log("Resquest Body: ", req.body);
+
     let user;
 
     if (validator.isEmail(identifier)) {
@@ -68,7 +64,6 @@ export const loginUserController = async (req, res) => {
       user = await User.findOne({ phone: identifier });
     }
 
-    console.log("User", user);
     if (!user) {
       return res.status(401).json({
         message: "Invalid Credentials",
@@ -83,15 +78,12 @@ export const loginUserController = async (req, res) => {
     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
       expiresIn: "1h",
     });
-    console.log("Token Check: ", token);
-    res
-      .status(200)
-      .json({
-        token,
-        userId: user._id,
-        user: user,
-        message: "Logged In Succesfully",
-      });
+    res.status(200).json({
+      token,
+      userId: user._id,
+      user: user,
+      message: "Logged In Succesfully",
+    });
   } catch (error) {
     res.status(500).json({
       message: "Server Error, Login User Failed",
